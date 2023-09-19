de6623cdb25a256d748d3a6c6fc2c005e358eb3f
juajodiaz04 <juanj.diaz1@udea.edu.co>
2023-09-19 09:01:13 -0500

    Verification Function

--- Sketch1_Try/Sketch1_Try.ino
+++ Sketch1_Try/Sketch1_Try.ino
@@ -1,18 +1,26 @@
 // C++ code
 //
-int latchPin = 12;  //Pin connected to ST_CP of 1st 74595
-int clockPin = 13;  //Pin connected to SH_CP of 1st 74595
-int dataPin = 11;   //Pin connected to DS of 1st 74595
+int latchPin = 12;  //Pin conectado a ST_CP del primer 74595
+int clockPin = 13;  //Pin conectado a SH_CP del primer 74595
+int dataPin = 11;   //Pin conectado a DS del primer 74595
 
-int latchPin2 = 6;  //Pin connected to ST_CP of 2nd 74595
-int clockPin2 = 7;  //Pin connected to SH_CP of 2nd 74595
-int dataPin2 = 5;   //Pin connected to DS of 2nd 74595
+int latchPin2 = 6;  //Pin conectado a ST_CP del segundo 74595
+int clockPin2 = 7;  //Pin connected to SH_CP del segundo 74595
+int dataPin2 = 5;   //Pin connected to DS del segundo 74595
 
 int j=0;
 int anode_decimal[8]={1, 128,64,32,16,8,4,2};
 int cathode_decimal[8]={254, 253, 251, 247, 239, 223, 191, 127};
+
+//Funciones
+void verificacion();
+
 void setup() {
-  //set pins to output so you can control the shift register
+  
+  //Iniciar el puerto serial para poder pedir entradas
+  Serial.begin(9600);
+  
+  //Los pines conectados al 74595 son de salida para controlar el integrado
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
@@ -23,25 +31,49 @@ void setup() {
 }
 
 void loop() {
+  verificacion();
+  
+  /*
     for(j=0;j<8;j++)
     {
-      /************To increase the ON time of LEDs five times more than 
-      OFF time to increase the brightness of LEDs*************/
- 
-      /*************************  TURN ON DIAGONAL LEDs ONLY  ***************************/  
-   
-      // take the latchPin low so the LEDs don't change while you're sending in bits:  
+      
+      //latchPin en bajo para poder modificar la trama de bits a enviar  
       digitalWrite(latchPin, LOW);
       shiftOut(dataPin, clockPin, MSBFIRST, cathode_decimal[j]);
       // shift out the bits:    
       digitalWrite(latchPin, HIGH);
   
-      // take the latchPin low so the LEDs don't change while you're sending in bits:    
+      //latchPin en bajo para poder modificar la trama de bits a enviar   
       digitalWrite(latchPin2, LOW);
       shiftOut(dataPin2, clockPin2, MSBFIRST, anode_decimal[j]);
       // shift out the bits:  
       digitalWrite(latchPin2, HIGH);
         
-      delay(100);                   
+      delay(1000);                   
    }
+   */
+  
+}
+
+
+void verificacion(){
+  
+  for (int i = 0; i < 5; i++){
+    
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, MSBFIRST, 0);
+    digitalWrite(latchPin, HIGH);
+    digitalWrite(latchPin2, LOW);
+    shiftOut(dataPin2, clockPin2, MSBFIRST, 255);
+    digitalWrite(latchPin2, HIGH);
+    delay(3000);
+    
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, MSBFIRST, 255);
+    digitalWrite(latchPin, HIGH);
+    digitalWrite(latchPin2, LOW);
+    shiftOut(dataPin2, clockPin2, MSBFIRST, 0);
+    digitalWrite(latchPin2, HIGH);
+    delay(3000);
+  }
 }
