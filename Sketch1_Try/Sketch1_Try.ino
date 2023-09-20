8e9e39cb867c15fd10103ada56d610abd1ecd90e
juajodiaz04 <juanj.diaz1@udea.edu.co>
2023-09-19 23:40:03 -0500

    Actualización entradas puerto serial

--- Sketch1_Try/Sketch1_Try.ino
+++ Sketch1_Try/Sketch1_Try.ino
@@ -1,8 +1,14 @@
 // C++ code
 //
-int latchPin=11;
-int clockPin=12;
-int dataPin=9;
+int latchPin=11; //Pin de Latch
+int clockPin=12; //Pin del Reloj	
+int dataPin=9;	//Pin de datos
+
+char receivedChar; //Para almacenar el valor del puerto serial
+
+unsigned long durat = 3000;
+unsigned long timestart;
+
 
 //Funciones
 void verificacion();
@@ -14,65 +20,122 @@ void arr_patt();
   
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+    
   pinMode(latchPin,OUTPUT);
   pinMode(clockPin,OUTPUT);
   pinMode(dataPin,OUTPUT);
+  
+  Serial.println("Escoge la opcion");
 }
 void loop() {
   // put your main code here, to run repeatedly:
 
-  verificacion();
+  //verificacion();
   //rect_patt();
   //X_patt();
   //alt_patt();
   //arr_patt();
 
-}
-
-void verificacion(){
+ if (Serial.available() > 0) {  
+    	receivedChar = Serial.read();
+   
+   		if (receivedChar == 'A'){
+     		Serial.println("Verificacion");
+        	timestart = millis();
+          	Serial.println(timestart);
+     		verificacion(3, 2000); //Iteraciones y tiempo de delay
+   		}
+   		else if(receivedChar == 'B'){
+     		Serial.println("Patron 1");
+        	timestart = millis();
+          	Serial.print("Tiempo de inicio: ");
+          	Serial.println(timestart);
+     		rect_patt(durat, timestart);
+        }
+   		else if(receivedChar == 'C'){
+     		Serial.println("Patron 2");
+        	timestart = millis();
+          	Serial.print("Tiempo de inicio: ");
+          	Serial.println(timestart);
+     		X_patt(durat, timestart);
+        }
+   		else if(receivedChar == 'D'){
+     		Serial.println("Patron 3");
+        	timestart = millis();
+          	Serial.print("Tiempo de inicio: ");
+          	Serial.println(timestart);
+     		alt_patt(durat, timestart);
+        }
+   		else if(receivedChar == 'E'){
+     		Serial.println("Patron 4");
+        	timestart = millis();
+          	Serial.print("Tiempo de inicio: ");
+          	Serial.println(timestart);
+     		arr_patt(durat, timestart);
+        }
+   		else {
+   		}
+   
+ } //Cierre if puerto serial
   
-  for (int i = 0; i < 5; i++){
+} //Cierre void loop
+
+void verificacion(int iter , int delays){
+  
+  for (int i = 0; i < iter; i++){
     
     shiftOut(dataPin,clockPin,MSBFIRST,0B00000000); 
     shiftOut(dataPin,clockPin,MSBFIRST,0B11111111); 
   	digitalWrite(latchPin,HIGH);
     digitalWrite(latchPin,LOW);
-    delay(3000);
+    delay(delays);
     
     shiftOut(dataPin,clockPin,MSBFIRST,0B11111111); 
     shiftOut(dataPin,clockPin,MSBFIRST,0B00000000);
     digitalWrite(latchPin,HIGH);
     digitalWrite(latchPin,LOW);
-    delay(3000);
+    delay(delays);
   }
 }
 
-void rect_patt(){
-  
-    shiftOut(dataPin,clockPin,MSBFIRST,0B11100111); 
-    shiftOut(dataPin,clockPin,MSBFIRST,0B10000001); 
-  	digitalWrite(latchPin,HIGH);
-    digitalWrite(latchPin,LOW);
-  
-    shiftOut(dataPin,clockPin,MSBFIRST,0B11000011); 
-    shiftOut(dataPin,clockPin,MSBFIRST,0B01000010); 
-  	digitalWrite(latchPin,HIGH);
-    digitalWrite(latchPin,LOW);
-  
-    shiftOut(dataPin,clockPin,MSBFIRST,0B00000000); 
-    shiftOut(dataPin,clockPin,MSBFIRST,0B00011000); 
-  	digitalWrite(latchPin,HIGH);
-    digitalWrite(latchPin,LOW);
-  
-  	shiftOut(dataPin,clockPin,MSBFIRST,0B10000001); 
-    shiftOut(dataPin,clockPin,MSBFIRST,0B00100100); 
-  	digitalWrite(latchPin,HIGH);
-    digitalWrite(latchPin,LOW);
+void rect_patt(unsigned long dur , unsigned long inicio){
+  	
+  	while(millis() - inicio <= dur){
+      
+      shiftOut(dataPin,clockPin,MSBFIRST,0B11100111); 
+      shiftOut(dataPin,clockPin,MSBFIRST,0B10000001); 
+      digitalWrite(latchPin,HIGH);
+      digitalWrite(latchPin,LOW);
+
+      shiftOut(dataPin,clockPin,MSBFIRST,0B11000011); 
+      shiftOut(dataPin,clockPin,MSBFIRST,0B01000010); 
+      digitalWrite(latchPin,HIGH);
+      digitalWrite(latchPin,LOW);
+
+      shiftOut(dataPin,clockPin,MSBFIRST,0B00000000); 
+      shiftOut(dataPin,clockPin,MSBFIRST,0B00011000); 
+      digitalWrite(latchPin,HIGH);
+      digitalWrite(latchPin,LOW);
+
+      shiftOut(dataPin,clockPin,MSBFIRST,0B10000001); 
+      shiftOut(dataPin,clockPin,MSBFIRST,0B00100100); 
+      digitalWrite(latchPin,HIGH);
+      digitalWrite(latchPin,LOW);
+
+      shiftOut(dataPin,clockPin,MSBFIRST,0B11111111); 
+      shiftOut(dataPin,clockPin,MSBFIRST,0B00000000);
+      digitalWrite(latchPin,HIGH);
+      digitalWrite(latchPin,LOW);
+ 
+    }
 
 }
 
-void X_patt(){
+void X_patt(unsigned long dur , unsigned long inicio){
   
+  while(millis() - inicio <= dur){
+   
     digitalWrite(latchPin,LOW);
     shiftOut(dataPin,clockPin,MSBFIRST,0B01111110); 
     shiftOut(dataPin,clockPin,MSBFIRST,0B10000001); 
@@ -93,9 +156,18 @@ void X_patt(){
     shiftOut(dataPin,clockPin,MSBFIRST,0B00011000); 
   	digitalWrite(latchPin,HIGH);
     digitalWrite(latchPin,LOW);
+  
+  	shiftOut(dataPin,clockPin,MSBFIRST,0B11111111); 
+    shiftOut(dataPin,clockPin,MSBFIRST,0B00000000);
+    digitalWrite(latchPin,HIGH);
+    digitalWrite(latchPin,LOW);
+  }
 }
 
-void alt_patt(){
+void alt_patt(unsigned long dur , unsigned long inicio){
+  
+  while(millis() - inicio <= dur){
+    
     shiftOut(dataPin,clockPin,MSBFIRST,0B00110011); 
     shiftOut(dataPin,clockPin,MSBFIRST,0B01101101); 
   	digitalWrite(latchPin,HIGH);
@@ -105,9 +177,17 @@ void alt_patt(){
     shiftOut(dataPin,clockPin,MSBFIRST,0B11011011); 
   	digitalWrite(latchPin,HIGH);
     digitalWrite(latchPin,LOW);
+  
+  	shiftOut(dataPin,clockPin,MSBFIRST,0B11111111); 
+    shiftOut(dataPin,clockPin,MSBFIRST,0B00000000);
+    digitalWrite(latchPin,HIGH);
+    digitalWrite(latchPin,LOW);
+  }
 }
 
-void arr_patt(){
+void arr_patt(unsigned long dur , unsigned long inicio){
+  
+  while(millis() - inicio <= dur){
   
     digitalWrite(latchPin,LOW);
     shiftOut(dataPin,clockPin,MSBFIRST,0B01111110); 
@@ -129,4 +209,50 @@ void arr_patt(){
     shiftOut(dataPin,clockPin,MSBFIRST,0B00011110); 
   	digitalWrite(latchPin,HIGH);
     digitalWrite(latchPin,LOW);
+  
+  	shiftOut(dataPin,clockPin,MSBFIRST,0B11111111); 
+    shiftOut(dataPin,clockPin,MSBFIRST,0B00000000);
+    digitalWrite(latchPin,HIGH);
+    digitalWrite(latchPin,LOW);
+  }
 }
+
+  // Codigo para seleccionar opciones y que se ejecuten hasta que la entrada del puerto serial cambie
+  /*
+   if (Serial.available() > 0) {  
+    receivedChar = Serial.read();
+    
+    if (receivedChar == 'A') { 
+      while (receivedChar == 'A') {
+        rect_patt();
+        if (Serial.available() > 0) {
+          receivedChar = Serial.read();
+        }
+      }
+    }
+    else if (receivedChar == 'B') { 
+      while (receivedChar == 'B') {
+        X_patt();
+        if (Serial.available() > 0) {
+          receivedChar = Serial.read();
+        }
+      }
+    }
+    else if (receivedChar == 'C') { 
+      while (receivedChar == 'C') {
+        alt_patt();
+        if (Serial.available() > 0) {
+          receivedChar = Serial.read();
+        }
+      }
+    }
+    else if (receivedChar == 'D') { 
+      while (receivedChar == 'D') {
+        arr_patt();
+        if (Serial.available() > 0) {
+          receivedChar = Serial.read();
+        }
+      }
+    }
+  }
+  */
