0c2c0bf9ad17f66278db4d3f87d8993600ed357d
juajodiaz04 <juanj.diaz1@udea.edu.co>
2023-09-25 11:35:42 -0500

    Corrección de comentarios
    Además se añade la parte para pedir el número de veces que se encienden los leds en la función de verificación

--- Sketch1_Try/Sketch1_Try.ino
+++ Sketch1_Try/Sketch1_Try.ino
@@ -9,8 +9,8 @@ int dataPin=9;	//Pin de datos
 
 char receivedChar; //Para almacenar el valor del puerto serial
 
-unsigned long durat = 1500;
-unsigned long timestart;
+unsigned long durat = 1500; //Tiempo de prueba
+unsigned long timestart;  // Almacena justo el tiempo donde se comienza a ejecutar la función
 
 
 //Funciones
@@ -68,7 +68,7 @@ void imagen(unsigned long dur, int del) {
   int *im;
   im = new int [8]; // Array para almacenar los 8 valores decimales
 
-  //Almacenar las cadenas de 8 bits como numeros enteros
+  //Almacena las cadenas de 8 bits como numeros enteros
   for (int j = 0; j < 8; j++) {
     Serial.print("Ingrese una linea de 8 numeros con 0 para apagado y 1 para encendido en la fila ");
     Serial.print(j);
@@ -78,11 +78,12 @@ void imagen(unsigned long dur, int del) {
       // Espera hasta que se reciban 8 caracteres
     }
 
-    // Leer los 8 caracteres y convertirlos a un valor decimal
+    // Lee los 8 caracteres y los convierte a decimal
     int valorDecimal = 0;
     for (int i = 0; i < 8; i++) {
       char caracter = (char)Serial.read();
-      valorDecimal = valorDecimal * 2 + (caracter - '0');
+      valorDecimal = valorDecimal * 2 + (caracter - 48); //El 48 se resta para obtener 1 o 0 usando la tabla ASCII
+      // valorDecimal = 0 * 2 + (1 - 0) = 1 → Ejm si el primer caracter es 1.
     }
     
     // Almacenar el valor decimal en el array
@@ -93,22 +94,22 @@ void imagen(unsigned long dur, int del) {
   
   //Valor para los cátodos
   int *cat;
-  cat = new int [8];
+  cat = new int [8]; //Array para almacenar los valores de los cátodos
   
   for (int j = 0; j <8 ; j++)
   {
     if (im[j] != 0) //Solo enciende las lineas cuyo valor entero es diferente de 0
     {
-      cat[j] = 255-(ceil(pow(2,7-j))); 
-    }
+      cat[j] = 255-(ceil(pow(2,7-j))); //Pone un 0 en la línea si el valor entero del ánodo es diferente de 0
+    }                                  //El resto lo llena con 1
       else
       {
-        cat[j] = 255;
+        cat[j] = 255; //0B11111111 equivale a enviar una fila de 1s
       }
   }
   for(int l = 0; l < 3;l++){
     
-    unsigned long inicio = millis();
+    unsigned long inicio = millis(); //Tiempo durante el que se muestra el patrón 
     while(millis() - inicio <= dur){
       
       for(int k = 0; k<8; k++){
@@ -133,8 +134,10 @@ void imagen(unsigned long dur, int del) {
 
 void rect_patt(unsigned long dur , unsigned long inicio){
   
+  /* Solución utilizando una matríz dinámica de 2 filas y 4 columnas */ 
+
   int **pr; int a = 6;
-  pr = (new int* [2]); //Filas
+  pr = (new int* [2]); //Filas 
   
   pr[0] = new int [4]; pr[1] = new int [4]; //Reservar columnas
   
@@ -166,11 +169,13 @@ void rect_patt(unsigned long dur , unsigned long inicio){
 
 
 void X_patt(unsigned long dur, unsigned long inicio){
+
+  /* Solución utilizando una matríz dinámica de 2 filas y 4 columnas */ 
   
   int *p; int a = 6;
   p = new int [4];
   
-  p[0] = 126;  //0B01111110 → 126 Byte semilla para el register shifter de los cátodos
+  p[0] = 126;  //0B01111110 → 126 Byte semilla para el register shifter de los cátodos (línea superior e inferior)
   
   for (int j = 1; j<4 ;j++) //Llenar el array de las otras semillas
   {
@@ -195,7 +200,8 @@ void X_patt(unsigned long dur, unsigned long inicio){
 
 void alt_patt(unsigned long dur, unsigned long inicio) { 
   
-  /* La solución trivial en este caso ahorra código enormemente*/
+  /* La solución trivial en este caso ahorra código enormemente y por eficiencia
+  no tendría sentido utilizar un array para mostrar el patrón*/
 
   while (millis() - inicio <= dur) {
     patt_print(latchPin, dataPin ,clockPin, 51, 219); //0B00110011,0B11011011)
@@ -261,53 +267,56 @@ void publik(){
    
    		if (receivedChar == '1'){
      		Serial.println("Verificacion");
-            Serial.println("Ingrese cuantos segundos de delay: ");
-            while (Serial.available() < 1) {  }   // Espera hasta que se reciba 1 caracter
-            int del = Serial.parseInt();
-     		verificacion(3,1000*del); //duración
-          	Serial.println("Escoge una opcion nuevamente");
+        Serial.println("Ingrese cuantos segundos de delay: ");
+        while (Serial.available() < 1) {  }   // Espera hasta que se reciba 1 caracter
+        int del = Serial.parseInt();
+        Serial.println("Ingrese el numero de secuencias a mostrar: ");
+        while (Serial.available() < 1) {  }   // Espera hasta que se reciba 1 caracter
+        int iter = Serial.parseInt();
+        verificacion(iter,1000*del); //iteraciones y duración
+        Serial.println("Escoge una opcion nuevamente");
    		}
    		else if(receivedChar == '2'){
-     		Serial.println("Imagen por pantalla");
-            Serial.println("Ingrese cuantos segundos de delay: ");
-            while (Serial.available() < 1) {  }   // Espera hasta que se reciba 1 caracter
-            int del = Serial.parseInt();
-     		imagen(durat,1000*del); //duración
-          	Serial.println("Escoge una opcion nuevamente");
+        Serial.println("Imagen por pantalla");
+        Serial.println("Ingrese cuantos segundos de delay: ");
+        while (Serial.available() < 1) {  }   // Espera hasta que se reciba 1 caracter
+        int del = Serial.parseInt();
+        imagen(durat,1000*del); //duración
+        Serial.println("Escoge una opcion nuevamente");
         }
    		else if(receivedChar == '3'){
-     		Serial.println("Patrones");
-            Serial.println("Ingrese cuantos segundos de delay: ");
-            while (Serial.available() < 1) {  }   // Espera hasta que se reciba 1 caracter
-            int del = Serial.parseInt();
-            for(int i = 0; i < 3; i++)
-            {
-              timestart = millis();
-              rect_patt(durat, timestart);
-              delay(1000*del);
+        Serial.println("Patrones");
+        Serial.println("Ingrese cuantos segundos de delay: ");
+        while (Serial.available() < 1) {  }   // Espera hasta que se reciba 1 caracter
+        int del = Serial.parseInt();
+        for(int i = 0; i < 3; i++)
+        {
+          timestart = millis();
+          rect_patt(durat, timestart);
+          delay(1000*del);
 
-              timestart = millis();
-              X_patt(durat, timestart);
-              delay(1000*del);
+          timestart = millis();
+          X_patt(durat, timestart);
+          delay(1000*del);
 
-              timestart = millis();
-              alt_patt(durat, timestart);
-              delay(1000*del);
+          timestart = millis();
+          alt_patt(durat, timestart);
+          delay(1000*del);
 
-              timestart = millis();
-              arr_patt(durat, timestart);
-              delay(1000*del);
-            }
-          	Serial.println("Escoge una opcion nuevamente");
+          timestart = millis();
+          arr_patt(durat, timestart);
+          delay(1000*del);
+        }
+        Serial.println("Escoge una opcion nuevamente");
         }
     
     	else if (receivedChar == '0'){
      		Serial.println("Has salido del programa");
-          	abort();
+        abort();
    		}
     
-        else {
-        }
+      else {
+      }
   }
 
 }
